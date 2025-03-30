6b0a688892d61c06ce89d6a69c63b4bb6172539b
Nik Volkov <freezemandix@ya.ru>
2025-03-30 16:58:09 +0700

    WIP: refactor from CSS to Qt native

--- src/genui/ui_widgets/window_mixins/prompt.py
+++ src/genui/ui_widgets/window_mixins/prompt.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QDialog, QFileDialog, QLabel
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QDialog, QFileDialog
+from PyQt6.QtCore import Qt, QSize, QRect
+from PyQt6.QtGui import QColor, QBrush, QPainter
 
 from ...ui_widgets.editor_autocomplete import AutoCompleteTextEdit
 from ...ui_widgets.lora_table import LoraTable
@@ -13,42 +14,60 @@ class SquareButton(QPushButton):
         return QSize(side, side)
         
         
-class CircleButton(QPushButton):
-    def __init__(self, text, number, color, parent=None):
-        super().__init__(parent)
-        
-        # container = QWidget()
-        layout = QHBoxLayout()
-        layout.setContentsMargins(10, 5, 10, 5)
-        layout.setSpacing(8)
-        
-        size = super().sizeHint()
-        print(size.height())
-        fsize = super().font().pointSizeF()
-        print(fsize)
-        s = 20
-        self.circle_label = QLabel(str(number))
-        self.circle_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.circle_label.setFixedSize(s, s)
-        self.circle_label.setStyleSheet(f"""
-            background-color: {color};
-            border-radius: 3px;
-            color: white;
-            font-weight: bold;
-            font-size: {fsize*1}pt;
-        """)
-        
-        text_label = QLabel(text)
-        
-        layout.addWidget(text_label)
-        layout.addWidget(self.circle_label)
-        layout.addStretch()
-        
-        self.setLayout(layout)
-        self.setFixedHeight(28)
+class CounterButton(QPushButton):
+    text_margin = 5
     
-    def setNumber(self, number):
-        self.circle_label.setText(str(number))
+    def __init__(self, text, number, color, parent=None):
+        super().__init__(text, parent)
+        
+        self.number = number
+    
+    def setNumber(self, number: int):
+        self.number = number
+        self.update()   # Emit paintEvent
+        
+    @property
+    def _counter_radius(self) -> int:
+        return self.height()//2
+        
+    @property
+    def _counter_rect(self) -> QRect:
+        text_width = self.fontMetrics().horizontalAdvance(self.text())
+        text_start = (self.width() - text_width) // 2
+        radius = self._counter_radius
+        x = text_start + text_width + self.text_margin
+        print(f"{self.width()=}; {text_width=}; {text_start=}; {x=}")
+        y = (self.height()-radius)//2
+        
+        center = (x, y)
+        size = (radius, radius)
+        return QRect(*center, *size)
+        
+    @property
+    def color(self) -> QColor:
+        color = "gray" if self.number == 0 else "#f0a04b"
+        return QColor(color)
+        
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        
+        font = painter.font()
+        font.setPixelSize(self._counter_radius)
+        painter.setFont(font)
+        
+        paint_rect = self._counter_rect
+        
+        painter.setBrush(QBrush(self.color))
+        painter.drawEllipse(paint_rect)
+        
+        painter.setPen(QColor("white"))
+        painter.drawText(
+            paint_rect, 
+            Qt.AlignmentFlag.AlignCenter, str(self.number)
+        )
 
 
 class LoraWindow(QDialog):
@@ -88,7 +107,7 @@ class PromptMixin:
         
         self.lora_window = LoraWindow()
         
-        self.open_lora_window_btn = CircleButton("LoRA", 0, "gray")
+        self.open_lora_window_btn = CounterButton("LoRA", 0, "gray")
         self.open_lora_window_btn.clicked.connect(self.lora_window.show)
         
         self.lora_window.lora_table.updated.connect(self.open_lora_window_btn.setNumber)
